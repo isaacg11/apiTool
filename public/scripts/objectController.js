b33e5c8fba33d220a3dc6fd1fff0007552cd602b
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-11-08 16:43:03 -0800

    update object layout complete

--- public/scripts/objectController.js
+++ public/scripts/objectController.js
@@ -3,6 +3,12 @@ var app = angular.module('stamplay', ['ngStamplay','angular-clipboard']);
 //CONTROLLER
 app.controller('objectController', ["$scope", "$stamplay", "appFactory", function($scope, $stamplay, appFactory){
 
+
+jQuery('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+});
+
 $scope.textToCopyCreate = "https://[appid].stamplayapp.com/api/user/v1/users";
  
 $scope.success = function () {
