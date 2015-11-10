db720774259d6f1edc5d1de739673481c06a5551
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-11-10 11:10:25 -0800

    basic layout for query object complete

--- public/scripts/objectController.js
+++ public/scripts/objectController.js
@@ -3,14 +3,16 @@ var app = angular.module('stamplay', ['ngStamplay','angular-clipboard']);
 //CONTROLLER
 app.controller('objectController', ["$scope", "$stamplay", "appFactory", function($scope, $stamplay, appFactory){
 
-
+//DATE PICKER
 jQuery('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
 });
 
+//COPY TO CLIPBOARD
 $scope.textToCopyCreate = "https://[appid].stamplayapp.com/api/user/v1/users";
 $scope.textToCopyUpdate = "https://[appid].stamplayapp.com/api/user/v1/users";
+$scope.textToCopyQuery = "https://[appid].stamplayapp.com/api/user/v1/users";
  
 $scope.success = function () {
     Materialize.toast('Copied to clipboard!', 3000, 'rounded');
@@ -19,6 +21,7 @@ $scope.success = function () {
 $scope.fail = function (err) {
     console.error('Error!', err);
     };
+
 
 
 }]);
