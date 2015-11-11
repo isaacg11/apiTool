afa2990cc963c05763f4cab37d3fa6b26c683010
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-11-10 19:16:02 -0800

    query object works

--- public/scripts/objectController.js
+++ public/scripts/objectController.js
@@ -9,10 +9,10 @@ jQuery('.datepicker').pickadate({
     selectYears: 15 
 });
 
-//COPY TO CLIPBOARD
+//COPY TO CLIPBOARD FIELDS
 $scope.textToCopyCreate = "https://[appid].stamplayapp.com/api/cobject/v1/post";
-$scope.textToCopyUpdate = "https://[appid].stamplayapp.com/api/user/v1/users";
-$scope.textToCopyQuery = "https://[appid].stamplayapp.com/api/user/v1/users";
+$scope.textToCopyUpdate = "https://[appid].stamplayapp.com/api/cobject/v1/post/id";
+$scope.textToCopyQuery = "http://[appid].stamplayapp.com/api/cobject/v1/post?field=val";
 $scope.textToCopyRate = "https://[appid].stamplayapp.com/api/user/v1/users";
  
 $scope.success = function () {
@@ -49,13 +49,11 @@ $scope.createObject = function(){
 
   $http.post("https://apiapp.stamplayapp.com/api/cobject/v1/book", newObject).success(function(res){
     document.getElementById('consoleCursor').className = "hidden";
-    document.getElementById('consoleStatus').className = "";
-    document.getElementById('consoleBody').className = "";
-    document.getElementById('consoleRes').className = "";
-    $scope.status = res.status;
-    $scope.text = res.statusText;
-    $scope.body = newObject;
-    $scope.response = res;
+    document.getElementById('createConsoleStatus').className = "";
+    document.getElementById('createConsoleBody').className = "";
+    document.getElementById('createConsoleResponse').className = "";
+    $scope.createBody = newObject;
+    $scope.createResponse = res;
     $scope.createTitle = res.title;
     $scope.createAuthor = res.author;
     $scope.createDate = res.dt_create;
@@ -78,17 +76,43 @@ $scope.updateObject = function(){
   };
 
   objectFactory.editObject(newData).then(function(res){
+    document.getElementById('consoleCursor').className = "hidden";
+    document.getElementById('updateConsoleStatus').className = "";
+    document.getElementById('updateConsoleBody').className = "";
+    document.getElementById('updateConsoleResponse').className = "";
+    $scope.updateBody = newData;
+    $scope.updateResponse = res;
     $scope.updateTitle = res.title;
     $scope.updateAuthor = res.author;
     $scope.updateDate = res.dt_update;
     $scope.updateId = res._id;
     $scope.newTitle = "";
     $scope.newAuthor = "";
-
   });
 };
 
+//QUERY OBJECT
+$scope.queryObject = function(){
+  var queryParams = {
+    cuisine: $scope.queryCuisine,
+    city: $scope.queryCity
+  };
 
+  objectFactory.findObject(queryParams).then(function(res){
+    document.getElementById('queryConsoleCursor').className = "hidden";
+    document.getElementById('queryConsoleStatus').className = "";
+    document.getElementById('queryConsoleBody').className = "";
+    document.getElementById('queryConsoleResponse').className = "";
+    $scope.queryBody = queryParams;
+    $scope.queryResponse = res;
+    $scope.queryRestaurantOutput = res.data[0].restaurant;
+    $scope.queryCuisineOutput = res.data[0].cuisine;
+    $scope.queryCityOutput = res.data[0].city;
+    $scope.queryAddressOutput = res.data[0].address;
+    $scope.queryCuisine = "";
+    $scope.queryCity = "";
+  });
+};
 
 
 
@@ -107,6 +131,13 @@ app.factory('objectFactory', ["$http","$q", function($http, $q) {
     editObject: function(newData){
       var q = $q.defer();
       $http.put("https://apiapp.stamplayapp.com/api/cobject/v1/book/564273fad53d37e40ef1ae88", newData).success(function(res){
+        q.resolve(res);
+      });
+      return q.promise;
+    },
+    findObject: function(queryParams){
+      var q = $q.defer();
+      $http.get('http://apiapp.stamplayapp.com/api/cobject/v1/restaurant?where={"$and":[{"cuisine":"'+queryParams.cuisine+'"},{"city":"'+queryParams.city+'"}]}').success(function(res){
         q.resolve(res);
       });
       return q.promise;
